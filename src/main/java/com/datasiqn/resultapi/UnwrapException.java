70e7896bf6b577e97e31c967f59e44d5d4a2372f
DatAsianBoi123 <zach.su158@gmail.com>
2023-03-27 19:47:45 -0600

    added javadocs and fixed typo

--- src/main/java/com/datasiqn/resultapi/UnwrapException.java
+++ src/main/java/com/datasiqn/resultapi/UnwrapException.java
@@ -1,7 +1,14 @@
 package com.datasiqn.resultapi;
 
+/**
+ * Represents an exception that occurs when an illegal use of {@link Result#unwrap()} or {@link Result#unwrapError()} is called.
+ * <br><br>
+ * Throws "Attempted to call `Result#unwrap` on an err value" if you are calling {@link Result#unwrap()} on an {@code Error} result
+ * <br>
+ * Throws "Attempted to call `Result#unwrapError` on an ok value" if you are calling {@link Result#unwrapError()} on an {@code Ok} result
+ */
 public class UnwrapException extends RuntimeException {
     public UnwrapException(String call, String valType) {
-        super(String.format("Attempted to call `Result::%s` on an %s value", call, valType));
+        super(String.format("Attempted to call `Result#%s` on an %s value", call, valType));
     }
 }
